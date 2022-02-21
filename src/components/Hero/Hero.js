e485f1cc88ffc5799c8bf813274467d3904f36b2
Pedro <96238562+PedroVader@users.noreply.github.com>
2022-02-21 09:14:46 +0100

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => (
         <SectionText>
         Nos centramos en la optimización de los motores de búsqueda y el desarrollo web para proporcionar a nuestros clientes las <Destacado> soluciones más viables. </Destacado>
         </SectionText>
-        <Button onClick={props.handleClick}>¡Quiero saber más!</Button>
+              
       </LeftSection>
     </Section>
   </>
